4ac5c5c9f7848b32c8d902a2d1ea084fd4dd9d6d
Arda Cetinkaya <ardacetinkaya@gmail.com>
2022-10-02 19:01:56 +0200

    Small refactor

--- src/environments/test/resources/api_gateways_deployments.tf
+++ src/environments/test/resources/api_gateways_deployments.tf
@@ -14,9 +14,3 @@ resource "aws_api_gateway_deployment" "deployment_01" {
     create_before_destroy = true
   }
 }
-
-resource "aws_api_gateway_stage" "dev_stage_01" {
-  deployment_id = aws_api_gateway_deployment.deployment_01.id
-  rest_api_id   = aws_api_gateway_rest_api.hello_lambda_api.id
-  stage_name    = "dev"
-}
--- src/environments/test/resources/api_gateways_stages.tf
+++ src/environments/test/resources/api_gateways_stages.tf
@@ -1,17 +1,5 @@
-resource "aws_api_gateway_stage" "stage_v1" {
+resource "aws_api_gateway_stage" "dev_stage_01" {
   deployment_id = aws_api_gateway_deployment.deployment_01.id
   rest_api_id   = aws_api_gateway_rest_api.hello_lambda_api.id
-  stage_name    = "v1"
-}
-
-resource "aws_api_gateway_stage" "stage_v2" {
-  deployment_id = aws_api_gateway_deployment.deployment_01.id
-  rest_api_id   = aws_api_gateway_rest_api.hello_lambda_api.id
-  stage_name    = "v2"
-}
-
-resource "aws_api_gateway_stage" "stage_v3" {
-  deployment_id = aws_api_gateway_deployment.deployment_01.id
-  rest_api_id   = aws_api_gateway_rest_api.hello_lambda_api.id
-  stage_name    = "v3"
+  stage_name    = "dev"
 }
