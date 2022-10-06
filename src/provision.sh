65fced908c276f5fd00c8bfb22cdeb0b64306081
Arda Cetinkaya <ardacetinkaya@gmail.com>
2022-10-06 21:39:26 +0200

    Test

--- src/provision.sh
+++ src/provision.sh
@@ -225,7 +225,7 @@ then
     terraform -chdir=${ENVIRONMENT_RESOURCES_FOLDER} init -upgrade=true -no-color -force-copy \
         -backend-config bucket=${STATE_FILE_STORAGE_NAME} \
         -backend-config region=${REGION} \
-        -backend-config key=${STATE_FILE_NAME} > $_outputFilePath
+        -backend-config key=${STATE_FILE_NAME} #> $_outputFilePath
 
 elif [ "${_command}" == 'plan' ]
 then
