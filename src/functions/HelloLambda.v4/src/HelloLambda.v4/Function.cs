2cb5f2c3db35746c20c6300e642e91daf6bc19ab
Arda Cetinkaya <ardacetinkaya@gmail.com>
2022-10-15 20:16:40 +0200

    Test

--- src/functions/HelloLambda.v4/src/HelloLambda.v4/Function.cs
+++ src/functions/HelloLambda.v4/src/HelloLambda.v4/Function.cs
@@ -1,4 +1,8 @@
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.Core;
+using Amazon.Runtime;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,19 +11,30 @@ namespace HelloLambda.v4;
 
 public class Function
 {
+    private readonly AmazonDynamoDBClient _client;
+    private readonly AmazonDynamoDBConfig _config;
+    private readonly AWSCredentials _AWSCredantials;
+    private readonly DynamoDBContext _context;
 
+    public Function()
+    {
+        _client = new AmazonDynamoDBClient();
+        _context = new DynamoDBContext(_client);
+    }
     /// <summary>
     /// A simple function that takes a string and returns both the upper and lower case version of the string.
     /// </summary>
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public bool FunctionHandler(Message message, ILambdaContext context)
+    public async Task<bool> FunctionHandler(Message message, ILambdaContext context)
     {
         try
         {
             if (message == null || string.IsNullOrEmpty(message.Text)) throw new ArgumentNullException(nameof(message));
             context.Logger.LogInformation($"Message is got: {message.Text})");
+            await _context.SaveAsync<Message>(message);
+            context.Logger.LogInformation($"Message is saved.");
             return true;
         }
         catch (System.Exception ex)
