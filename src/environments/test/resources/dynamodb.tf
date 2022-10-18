2bfd274c092e462a9704f75471da2c19755684d1
Arda Cetinkaya <ardacetinkaya@gmail.com>
2022-10-18 20:03:39 +0200

    dynamodb billing mode

--- src/environments/test/resources/dynamodb.tf
+++ src/environments/test/resources/dynamodb.tf
@@ -1,8 +1,6 @@
 resource "aws_dynamodb_table" "messages_table" {
   name           = "Messages"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 10
-  write_capacity = 10
+  billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "Id"
   range_key      = "Date"
 
