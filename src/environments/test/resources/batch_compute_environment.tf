6c2120e628b8153da707e08083cb7d092419cf4b
Arda Cetinkaya <ardacetinkaya@gmail.com>
2022-11-20 12:50:24 +0100

    Some revisions

--- src/environments/test/resources/batch_compute_environment.tf
+++ src/environments/test/resources/batch_compute_environment.tf
@@ -1,18 +1,3 @@
-data "aws_vpc" "main_network_01" {
-  filter {
-    name   = "tag:Name"
-    values = ["main_network"]
-  }
-}
-
-data "aws_subnet" "main_network_01_subnet_01" {
-  filter {
-    name   = "tag:Name"
-    values = ["main_subnet_#01"]
-  }
-}
-
-
 resource "aws_security_group" "some_batch_compute_01_sg_01" {
   name    = "some_batch_compute_01_sg_01"
   vpc_id  = data.aws_vpc.main_network_01.id
@@ -28,7 +13,7 @@ resource "aws_batch_compute_environment" "some_batch_compute_01" {
   compute_environment_name = "some_batch_compute_01"
 
   compute_resources {
-    max_vcpus = 16
+    max_vcpus = 8
 
     security_group_ids = [
       aws_security_group.some_batch_compute_01_sg_01.id
@@ -37,14 +22,68 @@ resource "aws_batch_compute_environment" "some_batch_compute_01" {
     subnets = [
       data.aws_subnet.main_network_01_subnet_01.id
     ]
-
     type = "FARGATE"
   }
 
   type         = "MANAGED"
+  
   depends_on = [
     aws_security_group.some_batch_compute_01_sg_01
   ]
+}
+
+resource "aws_batch_job_definition" "job_definition_01" {
+  name = "job_definition_01"
+  type = "container"
+  platform_capabilities = [
+    "FARGATE"
+  ]
+  
+  
+
+  container_properties = jsonencode({
+        image   = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/testartifacts:scheduled-v1-latest"
+        fargatePlatformConfiguration = {
+          platformVersion = "LATEST"
+        },
+        resourceRequirements = [
+          { type = "VCPU", value = "0.5" },
+          { type = "MEMORY", value = "1024" }
+        ],
+        executionRoleArn = aws_iam_role.task_execution_role.arn
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = aws_cloudwatch_log_group.job_definition_01_log_group_01.id
+            awslogs-region        = var.region
+            awslogs-stream-prefix = "batch"
+          }
+        }
+        networkConfiguration={
+          assignPublicIp = "ENABLED"
+        }
+      })
+}
+
+resource "aws_iam_role" "task_execution_role" {
+  name               = "task-exec"
+  assume_role_policy = data.aws_iam_policy_document.task_execution_role.json
+}
+
+data "aws_iam_policy_document" "task_execution_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_batch_job_queue" "job_queue_01" {
@@ -55,4 +94,9 @@ resource "aws_batch_job_queue" "job_queue_01" {
   compute_environments = [
     aws_batch_compute_environment.some_batch_compute_01.arn
   ]
+}
+
+resource "aws_cloudwatch_log_group" "job_definition_01_log_group_01" {
+  name              = "/aws/batch/job_definition_01"
+  retention_in_days = 1
 }
