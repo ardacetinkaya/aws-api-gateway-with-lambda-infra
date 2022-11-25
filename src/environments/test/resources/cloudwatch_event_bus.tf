bde0be6c0d2d8914410c7166ffd776f2f3bfd74e
Arda Cetinkaya <ardacetinkaya@gmail.com>
2022-11-25 23:13:54 +0100

    Some revisions

---
+++ src/environments/test/resources/cloudwatch_event_bus.tf
@@ -0,0 +1,3 @@
+resource "aws_cloudwatch_event_bus" "file_process_event_bus" {
+  name = "file-process-event-bus"
+}
--- src/environments/test/resources/cloudwatch_event_rule.tf
+++ src/environments/test/resources/cloudwatch_event_rule.tf
@@ -1,3 +1,5 @@
+###################################
+# Scheduled process rule
 resource "aws_cloudwatch_event_rule" "cloudwatch_event_rule" {
   name                = "batch-exec"
   schedule_expression = "cron(16,20,28 21-23 * * ? *)"
@@ -7,22 +9,26 @@ resource "aws_cloudwatch_event_rule" "cloudwatch_event_rule" {
 }
 
 resource "aws_cloudwatch_event_target" "batch_target" {
-  rule     = aws_cloudwatch_event_rule.cloudwatch_event_rule.name
-  role_arn = aws_iam_role.task_execution_role.arn
-  arn      = aws_batch_job_queue.job_queue_01.arn
+  rule              = aws_cloudwatch_event_rule.cloudwatch_event_rule.name
+  role_arn          = aws_iam_role.task_execution_role.arn
+  arn               = aws_batch_job_queue.job_queue_01.arn
+
   batch_target {
-    job_definition = aws_batch_job_definition.job_definition_01.arn
-    job_name       = "test01"
+    job_definition  = aws_batch_job_definition.job_definition_01.arn
+    job_name        = "test01"
   }
 
   provider = aws.primary-region
 }
+###################################
 
+###################################
+# S3 event process rule
 resource "aws_cloudwatch_event_rule" "cloudwatch_event_rule_01" {
-  name       = "batch-exec-by-file"
-  is_enabled = true
+  name              = "batch-exec-by-file"
+  is_enabled        = true
 
-  event_pattern = <<EOF
+  event_pattern     = <<EOF
 {
   "source": ["aws.s3"],
   "detail-type": ["Object Created"]
@@ -30,18 +36,19 @@ resource "aws_cloudwatch_event_rule" "cloudwatch_event_rule_01" {
 EOF
 
 
-  provider = aws.primary-region
+  provider          = aws.primary-region
 }
-
 
 resource "aws_cloudwatch_event_target" "cloudwatch_event_rule_01_target_01" {
-  rule     = aws_cloudwatch_event_rule.cloudwatch_event_rule_01.name
-  role_arn = aws_iam_role.task_execution_role.arn
-  arn      = aws_batch_job_queue.job_queue_01.arn
+  rule              = aws_cloudwatch_event_rule.cloudwatch_event_rule_01.name
+  role_arn          = aws_iam_role.task_execution_role.arn
+  arn               = aws_batch_job_queue.job_queue_01.arn
+
   batch_target {
-    job_definition = aws_batch_job_definition.job_definition_01.arn
-    job_name       = "test01"
+    job_definition  = aws_batch_job_definition.job_definition_01.arn
+    job_name        = "test01"
   }
 
-  provider = aws.primary-region
+  provider          = aws.primary-region
 }
+###################################
